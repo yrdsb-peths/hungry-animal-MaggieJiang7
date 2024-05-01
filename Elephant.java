dd7bfed8a57f039831915fca1519cb237d84b000
MaggieJiang7 <341050599@gapps.yrdsb.ca>
2024-05-01 10:12:43 -0400

    Added the Elephant Class

---
+++ Elephant.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The Elephant, main character
+ * 
+ * @author Maggie 
+ * @version May 2024
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        move(1);
+    }
+}
